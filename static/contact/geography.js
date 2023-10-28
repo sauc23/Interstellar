6e226ef13bc7707aa0aa4f284cbd8caab05bd11d
sss <89742348+sauc23@users.noreply.github.com>
2023-10-28 19:00:53 -0400

    Update geography.js

--- static/contact/geography.js
+++ static/contact/geography.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/reviews/',
-    bare: 'https://bare-server-vercel.vercel.app/bare/',
+    bare: 'https://cxfdfdfd.ddnsking.com/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/contact/algebra.js',
